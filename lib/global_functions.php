3956295c369599a3de76eb4c660e1def754b11cb
Andrzej Mazur <eznawca@gmail.com>
2021-07-18 22:32:52 +0200

    Adding descriptions to r() and rb() functions

--- lib/global_functions.php
+++ lib/global_functions.php
@@ -5,15 +5,20 @@
  */
 
 if (!function_exists('r')) {
-	function r($v, $o = '')
-	{
+	/**
+	 * Function better than var_dump()/print_r()
+	 */
+	function r($v, $o = '')	{
 		Eznawca\Comm\Comm::r($v, $o);
 	}
 }
 
 if (!function_exists('rb')) {
-	function rb($v, $o = '')
-	{
+	/**
+	 * Function better than var_dump()/print_r()
+	 * and terminate the current script
+	 */
+	function rb($v, $o = '') {
 		Eznawca\Comm\Comm::rb($v, $o);
 	}
 }
